27fdd21d67d86b7d5bf103a23b484a56f0e4acbc
Ryan Sethi <ryan@ryans-mbp.lan>
2022-12-19 19:24:30 +0000

    types done yuhhh

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Use Authentication in some way
 create favourite pokemon and add to somewhere
 Team Builder?
 show evolve levels?
+ctrl + shift + p
+
+types notes:
+
+notes for changing the colour: if statement containing ...
+if (pokemonInfo.types[0] === "normal"){
+// display the image card of the type...
+}
+
+Button onClick function that changes the colour based on name
--- my-app/src/Components/Input/input.js
+++ my-app/src/Components/Input/input.js
@@ -4,20 +4,23 @@ import Axios from "axios"
 
 function Input() {
     const [searchItem, setsearchItem] = useState("")
-    // const API_KEY = "33eb3c70"
     const [pokemonInfo, setPokemonInfo] = useState([])
+    // const [pokemonTypeImage, setPokemonTypeImage] = useState("")
+    const pokemonTypes = [{ background_color: "#aebe2f", type: "bug" }, { background_color: "#543d31", type: "dark" }, { background_color: "purple", type: "dragon" }, { background_color: "yellow", type: "electric" }, { background_color: "pink", type: "fairy" }, { background_color: "brown", type: "fighting" }, { background_color: "red", type: "fire" }, { background_color: "", type: "flying" }, { background_color: "", type: "ghost" }, { background_color: "", type: "grass" }, { background_color: "", type: "ground" }, { background_color: "", type: "ice" }, { background_color: "", type: "normal" }, { background_color: "", type: "poison" }, { background_color: "", type: "psychic" }, { background_color: "", type: "rock" }, { background_color: "", type: "steel" }, { background_color: "", type: "water" }]
 
     let text = searchItem;
     let lowerSearch = text.toLowerCase();
 
-    function handleClick() {
+    function handleClick(e) {
+
+
         //console.log(searchItem)
         let APIcallString = "https://pokeapi.co/api/v2/pokemon/" + lowerSearch
 
 
         Axios.get(APIcallString).then(function (response) {
             setPokemonInfo(response.data)
-            console.log(response)
+            console.log(response.data.types)
         }).catch(function (error) {
             console.log(error);
         })
@@ -26,14 +29,19 @@ function Input() {
     let imageNum = pokemonInfo.id
 
 
+    const onFormSubmit = e => {
+        e.preventDefault();
+
+    }
+
     return (
         <div>
-            <div className='buttonInput'>
-                <button onClick={handleClick}></button>
+            <form onSubmit={onFormSubmit} className='buttonInput' >
+                <button className='button' value="Add to do" type="submit" onClick={handleClick}></button>
                 <input type="text" onChange={(e) =>
                     setsearchItem(e.target.value)}></input>
 
-            </div>
+            </form>
             <>
 
                 {JSON.stringify(pokemonInfo) !== "[]" ?
@@ -48,8 +56,17 @@ function Input() {
                             <li>Speed:  {pokemonInfo.stats[5].base_stat}</li>
 
 
+
                         </ul>
                         <img className="pokemonImage" alt="pokemonImg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imageNum}.png`}></img>
+                        {pokemonInfo && pokemonInfo.types.map((pokemonObject) => {
+                            let indexOfType = pokemonTypes.findIndex((obj) => obj.type === pokemonObject.type.name)
+                            let pokemonTypeNames = pokemonTypes.map((obj) => obj.type)
+                            console.log(pokemonTypeNames)
+                            return pokemonTypeNames.includes(pokemonObject.type.name) && < button style={{ background: pokemonTypes[indexOfType].background_color }} className="typesImageClass" id="typesImage"   >{pokemonObject.type.name}</button>
+
+                        })}
+
                     </div>
                     :
                     <p>Press search to see Pokemon Info</p>
